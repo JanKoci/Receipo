215b636708201d6b60cd115a8e7009fefcba0ba4
JanKoci <hozakoci@gmail.com>
2020-11-18 23:14:00 +0100

    items page

---
+++ app/src/main/java/com/example/receipo/model/ReceiptItem.kt
@@ -0,0 +1,6 @@
+package com.example.receipo.model
+
+class ReceiptItem(val description: String, val price: Double) {
+
+    fun addImage() {}
+}
--- app/src/main/java/com/example/receipo/ui/creation/ItemsFragment.kt
+++ app/src/main/java/com/example/receipo/ui/creation/ItemsFragment.kt
@@ -5,13 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.receipo.R
+import com.example.receipo.model.ReceiptItem
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class ItemsFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private lateinit var recyclerView: RecyclerView
+//    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewAdapter: ItemsAdapter
+    private lateinit var viewManager: RecyclerView.LayoutManager
+    private var itemsList: MutableList<ReceiptItem> = mutableListOf()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,6 +26,28 @@ class ItemsFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val root = inflater.inflate(R.layout.fragment_items, container, false)
+
+        viewManager = LinearLayoutManager(root.context)
+        viewAdapter = ItemsAdapter(itemsList)
+
+        recyclerView = root.findViewById<RecyclerView>(R.id.items_recycle_view).apply {
+            layoutManager = viewManager
+            adapter = viewAdapter
+        }
+
+        val fab: FloatingActionButton = root.findViewById<FloatingActionButton>(R.id.items_fab)
+        fab.setOnClickListener { view -> addNewItem(view) }
+
+        recyclerView.addItemDecoration(
+            DividerItemDecoration(recyclerView.context, DividerItemDecoration.VERTICAL)
+        )
+
         return root
+    }
+
+    private fun addNewItem(view: View?) {
+        val newItem = ReceiptItem("", 0.0)
+        itemsList.add(newItem)
+        viewAdapter.notifyItemInserted(itemsList.size - 1)
     }
 }
