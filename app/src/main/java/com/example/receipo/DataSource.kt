c1ed971f922bd80fa94950b112a36206a9e5b454
Zubalík Petr <petr.zubalik@artin.cz>
2020-11-21 16:14:36 +0100

    Revert "detail acitivity overall - huge shit"
    This reverts commit 745d36fb357d05939cbac86a5c50157d9b72a297.

--- app/src/main/java/com/example/receipo/DataSource.kt
+++ app/src/main/java/com/example/receipo/DataSource.kt
@@ -7,19 +7,11 @@ class DataSource(val context: Context) {
         return MyData(context.resources.getStringArray(R.array.shops_array),
                       context.resources.getStringArray(R.array.dates_array),
                       context.resources.getStringArray(R.array.items_array),
-                      context.resources.getStringArray(R.array.price_array),
-                      context.resources.getStringArray(R.array.creation_dates_array),
-                      context.resources.getStringArray(R.array.expiration_dates_array),
-                      context.resources.getStringArray(R.array.detail_items_array),
-                      context.resources.getStringArray(R.array.detail_prices_array))
+                      context.resources.getStringArray(R.array.price_array))
     }
 
     data class MyData(val shops: Array<String>,
                       val dates: Array<String>,
                       val items: Array<String>,
-                      val prices: Array<String>,
-                      val creation_dates: Array<String>,
-                      val expiration_dates: Array<String>,
-                      val detail_items_array: Array<String>,
-                      val detail_prices_array: Array<String>)
+                      val prices: Array<String>)
 }
